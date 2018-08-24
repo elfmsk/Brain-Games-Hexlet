0f5b84212704483e58de750d279b14205b68c800
elfmsk <elfmsk@mail.ru>
2018-08-24 20:49:17 +0300

    gameBalanceWithCom

--- src/games/gameBalance.js
+++ src/games/gameBalance.js
@@ -16,30 +16,30 @@ export default () => {
     const randomNumber = String(utils(10, 9999));
 
     const correctAnswer = (arg) => {
-      const sortArg = sortNum(arg);
-      const min = Number(sortArg[0]);
-      const max = Number(sortArg[sortArg.length - 1]);
-      let medium;
-      if (max - min < stopBalance) {
+      const sortArg = sortNum(arg); //Сортируем массив от меньшего к большему
+      const min = Number(sortArg[0]); // Берем с левого края массива наименьшее число
+      const max = Number(sortArg[sortArg.length - 1]); //аналогично с наибольшим числом
+      let medium; // эта переменная пригодиться в дальнейшем при подсчете
+      if (max - min < stopBalance) { // Основное условие при выполнений которого вернется правильный ответ
         const result = {
           answer: sortArg.join(''),
           question: `${randomNumber}`,
         };
         return result;
       }
-      if ((max + min) % 2 === 0) {
+      if ((max + min) % 2 === 0) { // Идея простая находим среднее арифметическое значение.
         medium = String((max + min) / 2);
-        deleteFirstLast(sortArg);
-        sortArg.splice(0, 0, medium, medium);
+        deleteFirstLast(sortArg); // удаляем старые значения из массива
+        sortArg.splice(0, 0, medium, medium); //добавляем новые значения
       } else {
         deleteFirstLast(sortArg);
         sortArg.push(String(((max + min) / 2) - 0.5));
         sortArg.push(String(((max + min) / 2) + 0.5));
       }
-      return correctAnswer(sortArg);
+      return correctAnswer(sortArg); // рекурсивно вызываем функцию передавая новый массив
     };
 
-    return correctAnswer(strMas(randomNumber));
+    return correctAnswer(strMas(randomNumber)); //Вызываем функцию которая находит правильный ответ. Передаем ей рандомное число преобразованое в массив.
   };
   return brainGame(balance, description);
 };
