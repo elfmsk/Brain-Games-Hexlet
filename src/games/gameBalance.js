bc3496a2475c9055e5f688250b31f380d9df57ad
elfmsk <elfmsk@mail.ru>
2018-08-24 20:53:23 +0300

    gameBalanceWithCom_0_0_1

--- src/games/gameBalance.js
+++ src/games/gameBalance.js
@@ -16,11 +16,12 @@ export default () => {
     const randomNumber = String(utils(10, 9999));
 
     const correctAnswer = (arg) => {
-      const sortArg = sortNum(arg); //Сортируем массив от меньшего к большему
+      const sortArg = sortNum(arg); // Сортируем массив от меньшего к большему
       const min = Number(sortArg[0]); // Берем с левого края массива наименьшее число
-      const max = Number(sortArg[sortArg.length - 1]); //аналогично с наибольшим числом
+      const max = Number(sortArg[sortArg.length - 1]); // аналогично с наибольшим числом
       let medium; // эта переменная пригодиться в дальнейшем при подсчете
-      if (max - min < stopBalance) { // Основное условие при выполнений которого вернется правильный ответ
+      if (max - min < stopBalance) { // Основное условие при выполнений которого вернется
+        // правильный ответ
         const result = {
           answer: sortArg.join(''),
           question: `${randomNumber}`,
@@ -30,7 +31,7 @@ export default () => {
       if ((max + min) % 2 === 0) { // Идея простая находим среднее арифметическое значение.
         medium = String((max + min) / 2);
         deleteFirstLast(sortArg); // удаляем старые значения из массива
-        sortArg.splice(0, 0, medium, medium); //добавляем новые значения
+        sortArg.splice(0, 0, medium, medium); // добавляем новые значения
       } else {
         deleteFirstLast(sortArg);
         sortArg.push(String(((max + min) / 2) - 0.5));
@@ -39,7 +40,8 @@ export default () => {
       return correctAnswer(sortArg); // рекурсивно вызываем функцию передавая новый массив
     };
 
-    return correctAnswer(strMas(randomNumber)); //Вызываем функцию которая находит правильный ответ. Передаем ей рандомное число преобразованое в массив.
+    return correctAnswer(strMas(randomNumber)); // Вызываем функцию которая находит правильный
+    // ответ. Передаем ей рандомное число преобразованое в массив.
   };
   return brainGame(balance, description);
 };
