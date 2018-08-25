415093d498caa54cbfaf7d8260f44c8c06250271
elfmsk <elfmsk@mail.ru>
2018-08-25 13:18:35 +0300

    gameProgression

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	npm install
 
 start:
-	npm run babel-node -- src/bin/brain-balance.js
+	npm run babel-node -- src/bin/brain-progression.js
 
 publish:
 	npm publish
--- src/games/Progression.js
+++ src/games/Progression.js
@@ -3,6 +3,29 @@ import randomNum from '../utils';
 
 const description = 'What number is missing in this progression?';
 
+const gameProgression = () => {
+  const start = randomNum(1, 99);
+  const prog = randomNum(1, 2);
+  const questunStep = randomNum(0, 6);
+  let resultStr = `${start} `;
+  let correctAnswer = '';
+  let step = start;
+  for (let count = 0; count < 9; count += 1) {
+    step += prog;
+    if (count === questunStep) {
+      resultStr += '.. ';
+      correctAnswer += `${step} ${step + prog}`;
+      step += prog;
+    } else {
+      resultStr += `${step} `;
+    }
+  }
+  const result = {
+    answer: correctAnswer,
+    question: `${resultStr}`,
+  };
 
+  return result;
+};
 
 export default () => brainGame(gameProgression, description);
